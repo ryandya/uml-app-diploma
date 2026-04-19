3ec913d0518c638f32370d074ab3b0e137b8adbb
Artem <r@gmail.com>
2026-04-19 22:19:04 +0700

    pan-pinch feature

--- src/components/imagePreview/imagePreview.jsx
+++ src/components/imagePreview/imagePreview.jsx
@@ -1,17 +1,54 @@
 import './imagePreview.css'
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export default function ImagePreview({ imageUrl, imageName }) {
+
     return (
         <div className="wrapper">
             <div className="container">
                 <h2>Предпросмотр изображения</h2>
-                {imageName ? (<span className='imgName'>{imageName}</span>) : (<span className='imgName'>Название файла</span>)}
+
+                <span className='imgName'>
+                    {imageName || "Название файла"}
+                </span>
+
                 <div className="preview_container">
                     {imageUrl ? (
-                        <img
-                            src={imageUrl}
-                            alt="Предпросмотр UML-диаграммы"
-                            className='preview_Image' />
+                        <TransformWrapper
+                            initialScale={1}
+                            minScale={0.7}
+                            maxScale={4}>
+                            {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                                <>
+                                    <div className="tools">
+                                        <button className='zoom' onClick={() => zoomIn()}>
+                                            <img src="./img/MagnifierAdd.svg" alt="Img" className="magnifier" />
+                                        </button>
+                                        <button className='zoom' onClick={() => zoomOut()}>
+                                            <img src="./img/MagnifierRemove.svg" alt="Img" className="magnifier" />
+                                        </button>
+                                        <button className='zoom' onClick={() => resetTransform()}>
+                                            <img src="./img/ArrowReset32Regular.svg" alt="Img" className="magnifier" />
+                                        </button>
+                                    </div>
+                                    <TransformComponent
+                                        wrapperStyle={{
+                                            width: "100%",
+                                            height: "100%",
+                                        }}
+                                        contentStyle={{
+                                            width: "100%",
+                                            height: "100%",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                        }}>
+                                        <img
+                                            src={imageUrl}
+                                            alt="image"
+                                            className="preview_Image" />
+                                    </TransformComponent>
+                                </>)}
+                        </TransformWrapper>
                     ) : (
                         <div className="placeholder">
                             Загрузите изображение для предпросмотра
